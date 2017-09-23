2408ba7e98eafb987c4c8a9a3b9e5551b931a98a
William Morel <morelwilliam@outlook.com>
2017-09-23 12:13:20 -0400

    .config Folder

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,152 @@
+/home/william/Music/Foo Fighters - Concrete and Gold/01 - Foo Fighters - T-Shirt.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/02 - Foo Fighters - Run.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/03 - Foo Fighters - Make It Right.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/04 - Foo Fighters - The Sky Is A Neighborhood.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/05 - Foo Fighters - La Dee Da.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/06 - Foo Fighters - Dirty Water.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/07 - Foo Fighters - Arrows.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/08 - Foo Fighters - Happy Ever After (Zero Hour).wav
+/home/william/Music/Foo Fighters - Concrete and Gold/09 - Foo Fighters - Sunday Rain.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/10 - Foo Fighters - The Line.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/11 - Foo Fighters - Concrete and Gold.wav
+/home/william/Music/Foo Fighters - Concrete and Gold/01 - Foo Fighters - T-Shirt.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/02 - Foo Fighters - Run.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/03 - Foo Fighters - Make It Right.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/04 - Foo Fighters - The Sky Is A Neighborhood.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/05 - Foo Fighters - La Dee Da.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/06 - Foo Fighters - Dirty Water.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/07 - Foo Fighters - Arrows.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/08 - Foo Fighters - Happy Ever After (Zero Hour).mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/09 - Foo Fighters - Sunday Rain.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/10 - Foo Fighters - The Line.mp3
+/home/william/Music/Foo Fighters - Concrete and Gold/11 - Foo Fighters - Concrete and Gold.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/01 - Sherane A.K.A. Master Splinters Daughter.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/02 - Bitch, Dant Kill My Vibe.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/03 - Backstreet Freestyle.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/04 - The Art Of Peer Pressure.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/05 - Money Trees (Feat. Jay Rock).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/06 - Poetic Justice (Feat. Drake).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/07 - Good Kid.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/08 - M.A.A.D City (Feat. MC Eiht).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/09 - Swimming Pools (Drank) (Extended Version).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/10 - Sing About Me, Im Dying Of Thirst.mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/11 - Real (Feat. Anna Wise Of Sonnymoon).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/12 - Compton (Feat. Dr. Dre).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/13 - The Recipe (Feat. Dr Dre) (Bonus Track).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/14 - Black Boy Fly (Bonus Track).mp3
+/home/william/Music/Kendrick_Lamar-Good_Kid_M.A.A.D_City-(Deluxe_Edition)-(RapGodFathers.info)/15 - Now Or Never (Feat. Mary J. Blige) (Bonus Track).mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/01. Wesley's Theory.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/02. For Free! (Interlude).mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/03. King Kunta.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/04. Institutionalized.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/05. These Walls.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/06. u.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/07. Alright.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/08. For Sale! (Interlude).mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/09. Momma.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/10. Hood Politics.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/11. How Much A Dollar Cost.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/12. Complexion (A Zulu Love).mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/13. The Blacker The Berry.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/14. You Ain't Gotta Lie (Momma Said).mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/15. i.mp3
+/home/william/Music/Kendrick Lamar -To Pimp a Butterfly (2015) [WEB]/16. Mortal Man.mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/01 BLOOD..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/02 DNA..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/03 YAH..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/04 ELEMENT..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/05 FEEL..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/06 LOYALTY. (FEAT. RIHANNA.).mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/07 PRIDE..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/08 HUMBLE..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/09 LUST..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/10 LOVE. (FEAT. ZACARI.).mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/11 XXX. (FEAT. U2.).mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/12 FEAR..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/13 GOD..mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/14 DUCKWORTH..mp3
+/home/william/Music/Logic - Everybody (2017)/12 Black SpiderMan (feat. Damian Lemar Hudson).mp3
+/home/william/Music/Under Pressure (Deluxe Version)/01 Intro.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/02 Soul Food.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/03 I'm Gone.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/04 Gang Related.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/05 Buried Alive.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/06 Bounce.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/07 Growing Pains III.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/08 Never Enough.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/09 Metropolis.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/10 Nikki.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/11 Under Pressure.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/12 Till the End.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/13 Driving Ms Daisy (feat. Childish.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/14 Now.mp3
+/home/william/Music/Under Pressure (Deluxe Version)/15 Alright (feat. Big Sean).mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/01 Illuminatro.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/02 Flexicution.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/03 The Jam.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/04 Slave II.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/05 A Word From Our Sponsor.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/06 Wrist (feat. Pusha T).mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/07 Super Mario World.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/08 Studio Ambience At Night_ Malibu.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/09 44 Bars.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/10 Slave.mp3
+/home/william/Music/Logic - Bobby Tarantino (2016) [MP3~320Kbps]~[Hunter] [FRG]/Logic - Bobby Tarantino (2016)/11 Deeper Than Money.mp3
+/home/william/Music/Logic - Everybody (2017)/01 Hallelujah.mp3
+/home/william/Music/Logic - Everybody (2017)/02 Everybody.mp3
+/home/william/Music/Logic - Everybody (2017)/03 Confess (feat. Killer Mike).mp3
+/home/william/Music/Logic - Everybody (2017)/04 Killing Spree (feat. Ansel Elgort).mp3
+/home/william/Music/Logic - Everybody (2017)/05 Take It Back.mp3
+/home/william/Music/Logic - Everybody (2017)/06 America (feat. Black Thought, Chuck D, Big Lenbo & NO I.D.).mp3
+/home/william/Music/Logic - Everybody (2017)/07 Ink Blot (feat. Juicy J).mp3
+/home/william/Music/Logic - Everybody (2017)/08 Mos Definitely.mp3
+/home/william/Music/Logic - Everybody (2017)/09 Waiting Room.mp3
+/home/william/Music/Logic - Everybody (2017)/10 1-800-273-8255 (feat. Alessia Cara & Khalid).mp3
+/home/william/Music/Logic - Everybody (2017)/11 Anziety (feat. Lucy Rose).mp3
+/home/william/Music/Logic - Everybody (2017)/13 AfricAryaN (feat. Neil deGrasse Tyson).mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/1. Broken Whiskey Glass.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/2. Big Lie.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/4. No Option.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/5. Cold.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/6. White Iverson.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/7. I Fall Apart.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/8. Patient.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/9. Go Flex.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/11. Too Young.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/13. Up There.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/14. Yours Truly, Austin Post.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/15. Leave.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/16. Hit This Hard.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/18. Feeling Whitney.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/17. Money Made Me Do It (feat. 2 Chainz).mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/3. Deja Vu feat. Justin Bieber.mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/10. Feel (feat. Kehlani).mp3
+/home/william/Music/Post Malone - Stoney (Deluxe Edition) (2016) Full Album Mp3 320Kbps Groo/12. Congratulations feat. Quavo.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/01. Pornography.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/02. Oh My Dis Side (feat. Quavo).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/03. 3500 (feat. Future & 2 Chainz).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/04. Wasted (feat. Juicy J).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/05. 90210.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/06. Pray 4 Love (feat. The Weeknd).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/07. Night Call (feat. Swae Lee & Chief Keef).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/08. Piss On Your Grave (feat. Kanye West).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/09. Antidote.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/10. Impossible.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/11. Maria I'm Drunk, (feat. Justin Bieber & Young Thug).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/12. Flying High (feat. Toro y Moi).mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/13. I Can Tell.mp3
+/home/william/Music/Travis Scott -  Rodeo [2015]/14. Apple Pie.mp3
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/01 the ends.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/02 way back.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/03 coordinate.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/04 through the late night.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/05 beibs in the trap.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/06 sdp interlude.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/07 sweet sweet.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/08 outside.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/09 goosebumps.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/10 first take.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/11 pick up the phone.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/12 lose.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/13 guidance.m4a
+/home/william/Music/Travis Scott Birds In The Trap Sing McKnight [iTunes] 2016 AAC/AAC/14 wonderful.m4a
---
+++ .config/cmus/playlist.pl
@@ -0,0 +1,2 @@
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/06 LOYALTY. (FEAT. RIHANNA.).mp3
+/home/william/Music/Kendrick Lamar - DAMN. (2017) [Mp3~320kbps]/Kendrick Lamar - DAMN. (2017)/06 LOYALTY. (FEAT. RIHANNA.).mp3
